6ae4568201fb9bb0fa503e45bce0e1c6dd3dd65a
Yaroslav <mozgovoj.yaroslav@mail.ru>
2022-10-10 00:03:11 +0300

    T3

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int number = new Random().Next(10, 1000);
+
+int MaxDigit(int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    if (firstDigit > secondDigit) return firstDigit;
+    return secondDigit;
+}
+
+int maxDigit = MaxDigit (number);
+Console.WriteLine($"наибоьшее число =>{maxDigit}");
